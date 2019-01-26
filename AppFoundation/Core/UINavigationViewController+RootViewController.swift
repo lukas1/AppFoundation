947917b410efd1b4fafac3c17bc99c6a01d9646a
Lukas Vyletel <lukas@decent.ch>
2019-01-26 20:18:32 +0100

    Add typedRootViewController extension to UINavigationController

--- AppFoundation/Core/UINavigationViewController+RootViewController.swift
+++ AppFoundation/Core/UINavigationViewController+RootViewController.swift
@@ -1,7 +1,11 @@
 import UIKit
 
 public extension UINavigationController {
-    var rootViewController : UIViewController? {
+    var rootViewController: UIViewController? {
         return self.viewControllers.first
+    }
+
+    func typedRootViewController<T: UIViewController>() -> T? {
+        return rootViewController as? T
     }
 }
--- Example/Screens/Third Screen/ThirdCoordinator.swift
+++ Example/Screens/Third Screen/ThirdCoordinator.swift
@@ -15,14 +15,9 @@ extension ThirdCoordinator : SegueHandler {
     func coordinator(for segue: UIStoryboardSegue, sender: Any?) -> Coordinator? { return nil }
 
     func coordinator(for viewController: UIViewController, sender: Any?) -> Coordinator? {
-        switch viewController {
-        case is UINavigationController:
-            return ((viewController as? UINavigationController)?
-                .rootViewController as? FourthViewController
-            ).map { FourthCoordinator(viewController: $0) }
-        default:
-            return nil
-        }
+        guard let destination: FourthViewController = (viewController as? UINavigationController)?
+            .typedRootViewController() else { return nil }
+        return FourthCoordinator(viewController: destination)
     }
 }
 
