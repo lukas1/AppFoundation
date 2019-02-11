54abf12e521e25a8f668c5ebeda5cb645de89ec2
Lukas Vyletel <lukas@decent.ch>
2019-02-11 09:28:07 +0100

    Add viewDidAppear RX extension to UIViewController

--- AppFoundation/Core/UIViewController+Rx.swift
+++ AppFoundation/Core/UIViewController+Rx.swift
@@ -8,8 +8,15 @@ public extension Reactive where Base: UIViewController {
         return ControlEvent(events: source)
     }
 
-    public var viewWillAppear: ControlEvent<Void> {
-        let source = self.methodInvoked(#selector(Base.viewWillAppear(_:))).map { _ in }
+    public var viewWillAppear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewWillAppear(_:)))
+            .map { $0.first as? Bool ?? false }
+        return ControlEvent(events: source)
+    }
+
+    public var viewDidAppear: ControlEvent<Bool> {
+        let source = self.methodInvoked(#selector(Base.viewDidAppear(_:)))
+            .map { $0.first as? Bool ?? false }
         return ControlEvent(events: source)
     }
 }
