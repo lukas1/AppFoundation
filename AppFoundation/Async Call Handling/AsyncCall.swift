1155be97a1dfa947894aa1345b847263de604649
Lukas Vyletel <lukas@decent.ch>
2019-05-24 10:32:23 +0200

    Add withErrorHandling to Observable type as well

--- AppFoundation/Async Call Handling/AsyncCall.swift
+++ AppFoundation/Async Call Handling/AsyncCall.swift
@@ -27,11 +27,11 @@ public extension PrimitiveSequence where Trait == SingleTrait {
         errorToDefaultEventTransformer: @escaping (Error) -> DefaultErrorEvent,
         performsDefaultErrorHandling: @escaping PerformsDefaultErrorHandling = { _ in true }
     ) -> Driver<Element> where DefaultErrorEvent: FoundationEvent {
-        return self.do(onSuccess: nil, onError: { error in
-            if performsDefaultErrorHandling(error) {
-                events.accept(errorToDefaultEventTransformer(error))
-            }
-        }).asDriver(onErrorRecover: { _ in Driver.never() })
+        return asObservable().withErrorHandling(
+            events: events,
+            errorToDefaultEventTransformer: errorToDefaultEventTransformer,
+            performsDefaultErrorHandling: performsDefaultErrorHandling
+        )
     }
 
     func safeAsync<DefaultErrorEvent>(
@@ -45,5 +45,19 @@ public extension PrimitiveSequence where Trait == SingleTrait {
                 errorToDefaultEventTransformer: errorToDefaultEventTransformer,
                 performsDefaultErrorHandling: performsDefaultErrorHandling
             )
+    }
+}
+
+public extension ObservableType {
+    func withErrorHandling<DefaultErrorEvent>(
+        events: PublishRelay<FoundationEvent>,
+        errorToDefaultEventTransformer: @escaping (Error) -> DefaultErrorEvent,
+        performsDefaultErrorHandling: @escaping PerformsDefaultErrorHandling = { _ in true }
+    ) -> Driver<Element> where DefaultErrorEvent: FoundationEvent {
+        return self.do(onNext: nil, onError: { error in
+            if performsDefaultErrorHandling(error) {
+                events.accept(errorToDefaultEventTransformer(error))
+            }
+        }).asDriver(onErrorRecover: { _ in Driver.never() })
     }
 }
